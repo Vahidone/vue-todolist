b007c5f618e61c361068080a66dfd6d99c759fdf
Vahidone <v.ban.roma@gmail.com>
2023-09-30 20:10:23 +0200

    feat: add sound effect for error/add/trash

--- js/main.js
+++ js/main.js
@@ -35,34 +35,50 @@ const { createApp } = Vue;
     },
 
     methods: {
+
+      // stopErrorSound() {
+      //   const errorSound = document.getElementById('errorSound');
+      //   errorSound.pause();
+      //   errorSound.currentTime = 0; // Ripristina la posizione di riproduzione all'inizio
+      // },
+
       addTask() {
         if (this.newTask.testo.length < 5) { 
           this.isError = true;
           setTimeout(() => {
             this.isError = false;
-          }, 2000);
+          }, 5000);
+
+          const Sound = document.getElementById('errorSound');
+          Sound.play();
+        
         } else {
           this.tasks.unshift({ ...this.newTask }); // ho aggiunto un nuovo oggetto al posto della stringa
           this.newTask.testo = ''; // cancella solo il testo
           this.isError = false;
+          const Sound = document.getElementById('addSound');
+          Sound.play();
         }
       },
 
+    
       removeTask(index) {
         const task = this.tasks[index];
         if (!task.done) {
           task.showDeleteError = true;
 
+          const Sound = document.getElementById('errorSound');
+          Sound.play();
+          
           setTimeout(() => {
             task.showDeleteError = false;
-          }, 2000);
-
+          }, 5000);
         } else {
           this.tasks.splice(index, 1);
+          const Sound = document.getElementById('trashSound');
+          Sound.play();
         }
       },
-
-    
 
     },
 
